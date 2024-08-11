e6be50295b5853b514f3bf03da785cd9f17a10d5
svbiz4u <136245961+svbiz4u@users.noreply.github.com>
2024-08-11 10:53:57 +0200

    com-11082024-1053

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,34 +1,16 @@
+
 function filterArray(numbers, value) {
 let newArray = [];
-
-
-for (const number of numbers) {
-// for (let i = 0; i < numbers.length; i += 1) {
-if (number < value) {
-return newArray.push(number);
-// // // // newArray.push('number')
-// //  console.log('test');
-//   }
-// console.log(numbers[i])
-}
-//   console.log(newArray);
-// console.log(newArray)
-
-// console.log(newArray)
+for (let i = 0; i < numbers.length; i += 1) {
+if (numbers[i] > value) {
+newArray.push(numbers[i])
 }
 }
-
-
+ return newArray
+}
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
-
-
-// const planets = ["Earth", "Mars", "Venus"];
-
-// for (const planet of planets) {
-//   console.log(planet);
-// }
